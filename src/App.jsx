6481bd2b2e317642516db06dc42e419ba76854c8
Manish <mshakya.dev@gmai.com>
2025-04-13 11:48:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-primary flex items-center justify-center'>
+      <h1 className='text-4xl text-white font-bold'>Hello Vite + React + Tailwind CSS!</h1>
+    </div >
+  )
+}
+
+export default App
---
+++ src/components/Maincontent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Maincontent = () => {
+    return (
+        <section>Maincontent</section>
+    )
+}
+
+export default Maincontent
